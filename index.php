1d3907f7138d4be657865b71a26ba1ad0e65421e
Luigigabriel02 <luigigabriel977@gmail.com>
2025-09-15 20:48:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,14 @@ use Src\Cupom;
 $estoque = new Estoque();
 $carrinho = new Carrinho();
 
-// Caso 1: Adicionar produto válido
 $produto = $estoque->buscarProduto(1);
 echo $carrinho->adicionarItem($produto, 2);
 
-// Caso 2: Tentar adicionar além do estoque
 $produto = $estoque->buscarProduto(3);
 echo $carrinho->adicionarItem($produto, 10);
 
-// Caso 3: Remover produto
 echo $carrinho->removerItem(1);
 
-// Caso 4: Aplicar cupom
 $produto = $estoque->buscarProduto(2);
 echo $carrinho->adicionarItem($produto, 1);
 
@@ -31,3 +27,4 @@ echo $carrinho->listarItens();
 
 $cupom = new Cupom("DESCONTO10", 0.1);
 echo "Total com desconto: R$ " . number_format($carrinho->aplicarCupom($cupom), 2, ',', '.') . "\n";
+
